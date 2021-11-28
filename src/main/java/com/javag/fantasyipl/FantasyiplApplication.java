63c38f8f99699da5a689340ce5845ab4c8cc3aec
unknown <m.rajaguru1993@gmail.com>
2021-11-28 16:47:55 +0530

    Initial Commit

---
+++ src/main/java/com/javag/fantasyipl/FantasyiplApplication.java
@@ -0,0 +1,13 @@
+package com.javag.fantasyipl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasyiplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantasyiplApplication.class, args);
+	}
+
+}
